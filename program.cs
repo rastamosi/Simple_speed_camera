35ba513b18feeff0e4816aa1460d3c5c37759c36
Mostafa Ghorbani <48104432+rastamosi@users.noreply.github.com>
2021-09-12 09:40:03 +0300

    Speed camera
    simple speed camera project where two inputs are asked from the user, first the speed limit and then the speed of the car passing. Depending on the input the console will print if that car is able to drive away or the driver's car should be suspended!

---
+++ program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+namespace SpeedCam
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           Console.Write("What is the speed limit? ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("What is the speed of this car? ");
+            var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (carSpeed < speedLimit)
+                Console.WriteLine("Ok");
+                
+            else
+            {
+                const int kmPerDemeritPoint = 5;
+                var demeritPoints = (carSpeed - speedLimit)/kmPerDemeritPoint;
+                
+                if (demeritPoints > 12)
+                    Console.WriteLine("License Suspended");
+                    
+                else
+                    Console.WriteLine("Demerit points: " + demeritPoints);
+                    
+            }
+        }
+
+
+    }
+
+}
+
+
+ 
